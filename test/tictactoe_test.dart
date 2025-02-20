dd220de3f1aa826746bb894f7e7281ba8dd8de6b
martinfrouin <mart-33@hotmail.fr>
2025-02-20 09:56:25 +0100

    Add unit tests

--- test/tictactoe_test.dart
+++ test/tictactoe_test.dart
@@ -4,28 +4,63 @@ import 'package:tictactoe/helpers/game.dart';
 import 'package:tictactoe/models/game.dart';
 
 void main() {
-  test('Check winner when multiple points are given', () {
-    String? winner = checkWinner([
-      Point(
-        name: '×',
-        animation: SizedBox(),
-      ),
-      Point(
-        name: '×',
-        animation: SizedBox(),
-      ),
-      Point(
-        name: '×',
-        animation: SizedBox(),
-      ),
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
-    ]);
+  Point xMove = Point(
+    name: '×',
+    animation: const SizedBox(),
+  );
 
-    expect(winner, '×');
+  Point oMove = Point(
+    name: 'o',
+    animation: const SizedBox(),
+  );
+
+  test('Check winner and end game when no points are given', () {
+    List<Point?> board = List.filled(9, null);
+
+    String? winner = checkWinner(List.filled(9, null));
+    bool isEnded = checkEndGame(board);
+
+    expect(winner, null);
+    expect(isEnded, false);
+  });
+
+  test('Check winner and end game when multiple points are given', () {
+    List<Point> board = [
+      xMove,
+      oMove,
+      xMove,
+      oMove,
+      xMove,
+      oMove,
+      xMove,
+      oMove,
+      xMove,
+    ].toList();
+
+    bool isEnded = checkEndGame(board);
+    String? winner = checkWinner(board);
+
+    expect(isEnded, true);
+    expect(winner, "×");
+  });
+
+  test('Check end game and winner when some points are given', () {
+    List<Point?> board = [
+      xMove,
+      xMove,
+      null,
+      oMove,
+      oMove,
+      null,
+      null,
+      null,
+      null,
+    ].toList();
+
+    bool isEnded = checkEndGame(board);
+    String? winner = checkWinner(board);
+
+    expect(isEnded, false);
+    expect(winner, null);
   });
 }
